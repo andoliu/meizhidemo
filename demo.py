b27d2a03052c4c1583f8302a2c3a44f05a614dda
andoliu <andoliu4101@163.com>
2018-08-22 23:09:13 +0800

    demoo

--- demo.py
+++ demo.py
@@ -11,7 +11,8 @@ user = []
 password = []
 port = []
 
-sql = "SELECT database_connectivity.connectivity_url, database_connectivity.connectivity_user, database_connectivity.connectivity_password, database_connectivity.connectivity_script FROM database_connectivity, database_management_system, client WHERE database_connectivity.database_management_system_id = database_management_system.database_management_system_id and database_management_system.logical_system_id = client.logical_system_id"
+# 此SQL主要是通过匹配吃数据库中的找出对应的数据库表，从而找到对应的数据库连接表中需要的字段
+sql = "SELECT database_connectivity.connectivity_url, database_connectivity.connectivity_user, database_connectivity.connectivity_password, database_connectivity.connectivity_script FROM database_connectivity, `database`, sink_database WHERE database_connectivity.database_management_system_id = `database`.database_management_system_id AND `database`.database_id = sink_database.sink_database_key"
 cur.execute(sql)
 for r in cur.fetchall():
     print(r)
@@ -27,30 +28,50 @@ database['host'] = host
 database['user'] = user
 database['password'] = password
 database['port'] = port
-
-
 print(database)
 
 
-# todo 获取数据要存储的数据池以及要存储的字段
-sql1 = "SELECT database_connectivity.connectivity_url, database_connectivity.connectivity_user, database_connectivity.connectivity_password, database_connectivity.connectivity_script FROM database_connectivity, database_management_system, client WHERE database_connectivity.database_management_system_id = database_management_system.database_management_system_id and database_management_system.logical_system_id = client.logical_system_id"
-cur.execute(sql)
+# todo 获取数据要存储的数据库以及要存储的字段
+# 此SQL是通过匹配池数据库的池数据库key和数据库中的数据库ID，得到数据库技术名
+sql1 = "SELECT `database`.database_tec_name FROM `database`, sink_database WHERE `database`.database_id = sink_database.sink_database_key"
+cur.execute(sql1)
+order_database = cur.fetchall()
 
-
-# todo 读取外来数据库并储存需要数据
-db1 = pymysql.connect(host='lab-ms-d1-ex.mysql.rds.aliyuncs.com', port=3306, user='temptest', passwd='1234qwer', db='datasink-2')
-for i in range(len(database)):
-    newdb = pymysql.connect(host=database[i, 'host'], port=database[i, 'port'], user=database[i, 'user'], passwd=database[i, 'password'])
-    cur1 = newdb.cursor()
-    newsql = "SELECT * FROM "
-    cur1.execute(newsql)
-    data = cur1.fetchall()
-    create_sql = "CREATE TABLE admin(id CHAR )"
-    cur.execute(create_sql)
-    insert_sql = "INSERT INTO admin_payinfo() VALUE()"
-    cur.executemany(insert_sql, data)
-
+sql2 = "SELECT database_table.table_tec_name FROM database_table, `database`, sink_database WHERE database_table.database_id = `database`.database_id AND `database`.database_id = sink_database.sink_database_key"
+cur.execute(sql2)
+order_table = cur.fetchall()
 
 
 cur.close()
 db.close()
+
+
+# todo 读取数据库并储存需要数据
+db1 = pymysql.connect(host='lab-ms-d1-ex.mysql.rds.aliyuncs.com', port=3306, user='temptest', passwd='1234qwer', db=order_database[0][0])
+cur1 = db1.cursor()
+for i in range(len(database)):
+    print(type(database.loc[i, 'port']))
+    newdb = pymysql.connect(host=database.loc[i, 'host'], port=int(database.loc[i, 'port']), user=database.loc[i, 'user'], passwd=database.loc[i, 'password'])
+    newcur = newdb.cursor()
+    newcur.execute('SHOW DATABASES')
+    print(newcur.fetchall())
+    for x in order_table:
+        print(x)
+        # sql3 = "CREATE TABLE %s" %(x[0])
+        x1 = x[0].replace('-', '')
+        x1 = x1.replace('_', '')
+        print(x1)
+        sql4 = "INSERT INTO db1.%s SELECT * FROM newdb.%s" % (x1, x1)
+        cur1.execute(sql4)
+        # newsql = "SELECT * FROM %s" % x
+        # # newcur.execute(newsql)
+        # a = pd.read_sql(newsql, newdb)
+        # col_name_list = [tuple[0] for tuple in newcur.description]
+        # data = newcur.fetchall()
+        # create_sql = "CREATE TABLE admin(id CHAR )"
+        # cur1.execute(create_sql)
+        # insert_sql = "INSERT INTO admin_payinfo() VALUE()"
+        # cur1.executemany(insert_sql, data)
+
+
+
